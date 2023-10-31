acd08a45ef1995569a08f20c94a52a339577b1c9
dphun123 <dennisphun321@gmail.com>
2023-10-30 17:44:24 -0700

    removed unnecessary files

--- src/api/server.py
+++ src/api/server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, exceptions
 from fastapi.responses import JSONResponse
 from pydantic import ValidationError
-from src.api import audit, carts, catalog, bottler, barrels, admin
+from src.api import diary
 import json
 import logging
 import sys
@@ -21,12 +21,7 @@ app = FastAPI(
     },
 )
 
-app.include_router(audit.router)
-app.include_router(carts.router)
-app.include_router(catalog.router)
-app.include_router(bottler.router)
-app.include_router(barrels.router)
-app.include_router(admin.router)
+app.include_router(diary.router)
 
 @app.exception_handler(exceptions.RequestValidationError)
 @app.exception_handler(ValidationError)
